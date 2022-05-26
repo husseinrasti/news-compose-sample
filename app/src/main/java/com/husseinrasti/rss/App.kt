9d3902da03aee0335c0425efcf3a16cb1db14611
Hussein Rasti <husseinrasti@gmail.com>
2022-05-26 17:29:50 +0430

    Create database and DI by dagger-hilt

---
+++ app/src/main/java/com/husseinrasti/rss/App.kt
@@ -0,0 +1,11 @@
+package com.husseinrasti.rss
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by Hussein Rasti on 5/26/22.
+ */
+@HiltAndroidApp
+class App : MultiDexApplication()
--- app/src/main/java/com/husseinrasti/rss/StartupActivity.kt
+++ app/src/main/java/com/husseinrasti/rss/StartupActivity.kt
@@ -2,8 +2,10 @@ package com.husseinrasti.rss
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class StartupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
