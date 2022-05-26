c1ebc77283c9885da353216560d3cd332453df8e
Hussein Rasti <husseinrasti@gmail.com>
2022-05-26 13:11:00 +0430

    Added build logic

---
+++ build-logic/build-core/src/main/kotlin/com/husseinrasti/build_core/BuildAndroidConfig.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022  The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.husseinrasti.build_core
+
+
+/**
+ * Created by Hussein Rasti on 2/22/22.
+ */
+object BuildAndroidConfig {
+    const val APPLICATION_ID = "com.husseinrasti.rss"
+
+    const val BUILD_TOOLS_VERSION = "31.0.0"
+    const val COMPILE_SDK_VERSION = 31
+    const val MIN_SDK_VERSION = 23
+    const val TARGET_SDK_VERSION = 31
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0.0 ($VERSION_CODE)"
+
+    const val SUPPORT_LIBRARY_VECTOR_DRAWABLES = true
+
+    const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+
+    const val API_KEY = "pub_77206dba4be9566d317bd9c37c0a71e34a59"
+    const val API_VERSION = "1"
+    const val BASE_URL = "https://newsdata.io/api/$API_VERSION/"
+
+    const val DATABASE_NAME = "RSS.db"
+    const val SHARED_PREF_NAME = "PrefRSS"
+}
